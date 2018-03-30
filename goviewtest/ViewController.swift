e0dba4393d0f0eba666ba028605190ac3993fef2
吴正涛 <wuzhengtao@wuzhengtaodemacbook-pro.local>
2018-03-30 19:38:50 +0800

    Initial Commit

---
+++ goviewtest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  goviewtest
+//
+//  Created by 吴正涛 on 2018/3/30.
+//  Copyright © 2018年 吴正涛. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
